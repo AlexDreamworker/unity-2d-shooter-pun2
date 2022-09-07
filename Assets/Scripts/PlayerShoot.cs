023a1c638725a7b4c33d542fc9afd1ddef914bfa
Alexey <94228620+AlexDreamworker@users.noreply.github.com>
2022-09-07 19:37:27 +0600

    Added GunHolder, weapons, projectiles and switch weapon logic for Player

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -4,43 +4,54 @@ namespace ShooterPun2D
 {
 	public class PlayerShoot : MonoBehaviour
 	{
-		[SerializeField] private Transform _gunHolder;
-		[SerializeField] private GameObject _projectile;
-		[SerializeField] private Transform _shootPoint;
-		[SerializeField] private float _projectileSpeed = 1200f;
-		[SerializeField] private float _fireRate = 8f;
+		[SerializeField] private GameObject _gunHolderGO;
 
-		private float _readyForNextShoot;
+		private GunHolder _gunHolderScript;
+
+		//[SerializeField] private GameObject _projectile;
+		//[SerializeField] private Transform _shootPoint;
+		//[SerializeField] private float _projectileSpeed = 1200f;
+		//[SerializeField] private float _fireRate = 8f;
+
+		//private float _readyForNextShoot;
 
 		private Vector2 _direction;
+
+		private void Awake()
+		{
+			_gunHolderScript = _gunHolderGO.GetComponent<GunHolder>();
+		}
 
 		private void Update()
 		{
 			Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			_direction = mousePosition - (Vector2)_gunHolder.position;
+			_direction = mousePosition - (Vector2)_gunHolderGO.transform.position;
 
 			FaceMouse();
 
 			if (Input.GetMouseButton(0)) 
 			{
-				if (Time.time > _readyForNextShoot) 
-				{
-					_readyForNextShoot = Time.time + 1 / _fireRate;
-					Shoot();
-				}
+				// if (Time.time > _readyForNextShoot) 
+				// {
+				// 	_readyForNextShoot = Time.time + 1 / _fireRate;
+				// 	Shoot();
+				// }
+
+				//todo: Refak this shit
+				_gunHolderScript.CurrentGun.GetComponent<Weapon>().Shoot();
 			}
 		}
 
 		private void FaceMouse() 
 		{
-			_gunHolder.transform.right = _direction;
+			_gunHolderGO.transform.right = _direction;
 		}
 
-		private void Shoot() 
-		{
-			GameObject projectile = Instantiate(_projectile, _shootPoint.position, _shootPoint.rotation);
-			projectile.GetComponent<Rigidbody2D>().AddForce(projectile.transform.right * _projectileSpeed);
-		}
+		// private void Shoot() 
+		// {
+		// 	GameObject projectile = Instantiate(_projectile, _shootPoint.position, _shootPoint.rotation);
+		// 	projectile.GetComponent<Rigidbody2D>().AddForce(projectile.transform.right * _projectileSpeed);
+		// }
 	}
 }
 
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,7 +4,19 @@ namespace ShooterPun2D
 {
 	public class Weapon : MonoBehaviour
 	{
-		
+		[SerializeField] private GameObject _projectile;
+		[SerializeField] private Transform _shootPoint;
+		[SerializeField] private float _projectileSpeed = 1200f;
+
+		//[SerializeField] private float _fireRate = 8f;
+
+		//private float _readyForNextShoot;
+
+		public void Shoot() 
+		{
+			GameObject projectile = Instantiate(_projectile, _shootPoint.position, _shootPoint.rotation);
+			projectile.GetComponent<Rigidbody2D>().AddForce(projectile.transform.right * _projectileSpeed);
+		}		
 	}
 }
 
