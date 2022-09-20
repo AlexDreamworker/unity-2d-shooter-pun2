825293416ee651dba8ddfc17dc27c6ff79ee27f6
Alexey <94228620+AlexDreamworker@users.noreply.github.com>
2022-09-20 22:20:26 +0600

    Updated HUD health, added test player death state

---
+++ Assets/pt_2/Scripts/Player/Hud/HealthWidget.cs
@@ -0,0 +1,43 @@
+using TMPro;
+using UnityEngine;
+
+namespace ShooterPun2D.pt2
+{
+	public class HealthWidget : MonoBehaviour
+	{
+		[SerializeField] private PlayerHealth _target;
+		[SerializeField] private TMP_Text _render;
+
+		//private Color color;
+
+		private void OnEnable()
+		{
+			_target.OnHealthChanged += UpdateValue;
+		}
+
+		private void OnDisable()
+		{
+			_target.OnHealthChanged -= UpdateValue;
+		}
+
+		public void UpdateValue(int value)
+		{
+			if (_target == null)
+				return;
+			
+			_render.text = value.ToString();
+
+			var result = value / 100f;
+
+			if (result > 0.7f)
+			{
+				_render.color = new Color(1 - result, result, 0);
+			}
+			else
+			{
+				_render.color = new Color(1, result, 0);
+			}
+		}
+	}
+}
+
--- Assets/pt_2/Scripts/Player/PlayerHealth.cs
+++ Assets/pt_2/Scripts/Player/PlayerHealth.cs
@@ -8,12 +8,41 @@ namespace ShooterPun2D.pt2
 	{
 		public event Action<int> OnHealthChanged;
 		[SerializeField] private int _currentHealth = 100;
+
+		//todo: refactoring?
+		[SerializeField] private GameObject _bloodParticle = null;
+		[SerializeField] private GameObject _chunkParticle = null;
+		[SerializeField] private SpriteRenderer _legs;
+		[SerializeField] private SpriteRenderer _torso;
+		[SerializeField] private GameObject _canvasOverhead;
+		
+		private PlayerMovement _movement;
+		private PlayerWeapon _weapon;
+		private Rigidbody2D _rigidbody;
+		private Collider2D _collider;
+		private PlayerInputReader _playerInput;
+
 		private int _maxHealth;
 		private PhotonView _photonView;
 
 		private void Awake()
 		{
 			_photonView = GetComponent<PhotonView>();
+			//todo: refactoring?
+			_rigidbody = GetComponent<Rigidbody2D>();
+			_collider = GetComponent<Collider2D>();
+			_playerInput = GetComponent<PlayerInputReader>();
+			_movement = GetComponent<PlayerMovement>();
+			_weapon = GetComponent<PlayerWeapon>();
+		}
+
+		private void Update()
+		{
+			if (_currentHealth <= 0) 
+			{
+				_photonView.RPC("Death", RpcTarget.All);
+				return;
+			}
 		}
 
 		private void Start()
@@ -33,7 +62,24 @@ namespace ShooterPun2D.pt2
 			OnHealthChanged?.Invoke(_currentHealth);
         }
 
-
+		[PunRPC]
+		private void Death() 
+		{
+			_bloodParticle.SetActive(true);
+			_chunkParticle.SetActive(true);
+			if (_photonView.IsMine) 
+			{
+				_rigidbody.bodyType = RigidbodyType2D.Kinematic;
+			}
+			_collider.enabled = false;
+			_playerInput.enabled = false;
+			_movement.enabled = false;
+			_weapon.enabled = false;
+			_canvasOverhead.SetActive(false);
+			_legs.enabled = false;
+			_torso.enabled = false;
+			return;
+		}
     }
 }
 
