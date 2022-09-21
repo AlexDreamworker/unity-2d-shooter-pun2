7d641a45518d35b5dbe5f16cafb1fa0657dfae2b
Alexey <94228620+AlexDreamworker@users.noreply.github.com>
2022-09-21 23:32:12 +0600

    Created PlayerBrain script, refactored PlayerHealth logic

---
+++ Assets/pt_2/Scripts/Player/PlayerBrain.cs
@@ -0,0 +1,83 @@
+using Photon.Pun;
+using UnityEngine;
+
+namespace ShooterPun2D.pt2
+{
+	public class PlayerBrain : MonoBehaviour
+	{
+		[SerializeField] private SpriteRenderer _bodyLegs;
+		[SerializeField] private SpriteRenderer _bodyTorso;
+		[SerializeField] private GameObject _canvasOverhead;
+		//[SerializeField] private GameObject _bloodParticle = null;
+		//[SerializeField] private GameObject _chunkParticle = null;
+
+		[SerializeField] private ParticleSystem _blood;
+		[SerializeField] private ParticleSystem _chunk;
+
+		private PhotonView _photonView;
+		private Rigidbody2D _rigidbody;
+		private Collider2D _collider;
+		private PlayerInputReader _playerInput;
+		private PlayerMovement _playerMovement;
+		private PlayerWeapon _playerWeapon;
+		private PlayerHealth _playerHealth;
+
+		private void Awake()
+		{
+			_photonView = GetComponent<PhotonView>();
+			_rigidbody = GetComponent<Rigidbody2D>();
+			_collider = GetComponent<Collider2D>();
+			_playerInput = GetComponent<PlayerInputReader>();
+			_playerMovement = GetComponent<PlayerMovement>();
+			_playerWeapon = GetComponent<PlayerWeapon>();
+			_playerHealth = GetComponent<PlayerHealth>();
+		}
+
+		private void OnEnable()
+		{
+			//if (!_photonView.IsMine)
+				//return;
+
+			_playerHealth.OnPlayerDead += Dying;
+			_blood.Stop();
+			_chunk.Stop();
+		}
+
+		private void OnDisable()
+		{
+			//if (!_photonView.IsMine)
+				//return;
+			
+			_playerHealth.OnPlayerDead -= Dying;
+		}
+
+		private void Dying()
+		{
+			_photonView.RPC("Death", RpcTarget.All);
+		}
+ 
+		[PunRPC]
+		private void Death() 
+		{
+			//_bloodParticle.SetActive(true);
+			//_chunkParticle.SetActive(true);
+
+			_blood.Play();
+			_chunk.Play();
+			
+			if (_photonView.IsMine) 
+			{
+				_rigidbody.bodyType = RigidbodyType2D.Kinematic;
+			}
+			_collider.enabled = false;
+			_playerInput.enabled = false;
+			_playerMovement.enabled = false;
+			_playerWeapon.enabled = false;
+			_canvasOverhead.SetActive(false);
+			_bodyLegs.enabled = false;
+			_bodyTorso.enabled = false;
+			return;
+		}
+	}
+}
+
--- Assets/pt_2/Scripts/Player/PlayerHealth.cs
+++ Assets/pt_2/Scripts/Player/PlayerHealth.cs
@@ -7,47 +7,65 @@ namespace ShooterPun2D.pt2
 	public class PlayerHealth : MonoBehaviour
 	{
 		public event Action<int> OnHealthChanged;
+		public event Action OnPlayerDead;
+
 		[SerializeField] private int _currentHealth = 100;
+		private int _maxHealth = 100;
+
 
 		//todo: refactoring?
-		[SerializeField] private GameObject _bloodParticle = null;
-		[SerializeField] private GameObject _chunkParticle = null;
-		[SerializeField] private SpriteRenderer _legs;
-		[SerializeField] private SpriteRenderer _torso;
-		[SerializeField] private GameObject _canvasOverhead;
+		//[SerializeField] private GameObject _bloodParticle = null;
+		//[SerializeField] private GameObject _chunkParticle = null;
+		//[SerializeField] private SpriteRenderer _legs;
+		//[SerializeField] private SpriteRenderer _torso;
+		//[SerializeField] private GameObject _canvasOverhead;
 		
-		private PlayerMovement _movement;
-		private PlayerWeapon _weapon;
-		private Rigidbody2D _rigidbody;
-		private Collider2D _collider;
-		private PlayerInputReader _playerInput;
+		//private PlayerMovement _movement;
+		//private PlayerWeapon _weapon;
+		//private Rigidbody2D _rigidbody;
+		//private Collider2D _collider;
+		//private PlayerInputReader _playerInput;
 
-		private int _maxHealth;
+		public int Health 
+		{
+			get => _currentHealth;
+			set 
+			{
+				_currentHealth = Mathf.Clamp(value, 0, _maxHealth);
+				if (_currentHealth <= 0) 
+				{
+					OnPlayerDead?.Invoke();
+				}
+			}
+		}
+
 		private PhotonView _photonView;
 
 		private void Awake()
 		{
 			_photonView = GetComponent<PhotonView>();
+			//_playerBrain = GetComponent<PlayerBrain>();
+
 			//todo: refactoring?
-			_rigidbody = GetComponent<Rigidbody2D>();
-			_collider = GetComponent<Collider2D>();
-			_playerInput = GetComponent<PlayerInputReader>();
-			_movement = GetComponent<PlayerMovement>();
-			_weapon = GetComponent<PlayerWeapon>();
+			//_rigidbody = GetComponent<Rigidbody2D>();
+			//_collider = GetComponent<Collider2D>();
+			//_playerInput = GetComponent<PlayerInputReader>();
+			//_movement = GetComponent<PlayerMovement>();
+			//_weapon = GetComponent<PlayerWeapon>();
 		}
 
-		private void Update()
-		{
-			if (_currentHealth <= 0) 
-			{
-				_photonView.RPC("Death", RpcTarget.All);
-				return;
-			}
-		}
+		// private void Update()
+		// {
+		// 	if (_currentHealth <= 0) 
+		// 	{
+		// 		_photonView.RPC("Death", RpcTarget.All);
+		// 		return;
+		// 	}
+		// }
 
 		private void Start()
 		{
-			OnHealthChanged?.Invoke(_currentHealth);
+			OnHealthChanged?.Invoke(Health);
 		}
 
 		public void TakeDamage(int value) 
@@ -58,28 +76,28 @@ namespace ShooterPun2D.pt2
         [PunRPC]
         private void Damage(int value) 
         {
-        	_currentHealth -= value;
-			OnHealthChanged?.Invoke(_currentHealth);
+        	Health -= value;
+			OnHealthChanged?.Invoke(Health);
         }
 
-		[PunRPC]
-		private void Death() 
-		{
-			_bloodParticle.SetActive(true);
-			_chunkParticle.SetActive(true);
-			if (_photonView.IsMine) 
-			{
-				_rigidbody.bodyType = RigidbodyType2D.Kinematic;
-			}
-			_collider.enabled = false;
-			_playerInput.enabled = false;
-			_movement.enabled = false;
-			_weapon.enabled = false;
-			_canvasOverhead.SetActive(false);
-			_legs.enabled = false;
-			_torso.enabled = false;
-			return;
-		}
+		// [PunRPC]
+		// private void Death() 
+		// {
+		// 	_bloodParticle.SetActive(true);
+		// 	_chunkParticle.SetActive(true);
+		// 	if (_photonView.IsMine) 
+		// 	{
+		// 		_rigidbody.bodyType = RigidbodyType2D.Kinematic;
+		// 	}
+		// 	_collider.enabled = false;
+		// 	_playerInput.enabled = false;
+		// 	_movement.enabled = false;
+		// 	_weapon.enabled = false;
+		// 	_canvasOverhead.SetActive(false);
+		// 	_legs.enabled = false;
+		// 	_torso.enabled = false;
+		// 	return;
+		// }
     }
 }
 
